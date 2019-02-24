704dc14e7949df5bb4a07c3927b8e2d0fba00912
web-commander <web.commander.gbdr@gmail.com>
2019-02-24 14:05:59 +0900

    フッターを追加

---
+++ footer.php
@@ -0,0 +1,10 @@
+        </div> <!-- row -->
+      </main>
+    </div> <!-- container  -->
+   <?php wp_footer(); 
+   ?>
+  </body>
+  <footer class="blog-footer">
+    <p>© 2019  <a href="">とあるEngineer</a></p>
+  </footer>
+</html>
